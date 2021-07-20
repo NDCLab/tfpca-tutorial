f20e81c3256fd7f01f6fa9f986f1eb951a52b737
yanbin-niu <79607547+yanbin-niu@users.noreply.github.com>
2021-07-20 20:09:06 +0800

    Delete eeglab2021.0 directory

--- eeglab2021.0/plugins/erplab/functions/displayEquiCom.m
+++
@@ -1,4 +0,0 @@
-fprintf([repmat('-', 1,100) '\n'])
-cprintf([0.1333    0.5451    0.1333], '%%Equivalent command:\n');
-fprintf('%s\n', erpcom);
-fprintf([repmat('-', 1,100) '\n'])
--- eeglab2021.0/plugins/erplab/functions/strtrimx.m
+++
@@ -1,8 +0,0 @@
-% PURPOSE: Removes leading and trailing white space from string, and remove any
-%          duplicated white space in between the strings (if any) 
-%
-% Author: Javier Lopez-Calderon
-
-function string = strtrimx(string)
-string = strtrim(string);
-string = regexprep(string, '\s+',' ');
